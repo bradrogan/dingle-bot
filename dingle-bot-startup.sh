bf89fa02d8e47fe6f4ee0f5fee34c96f43238753
bradrogan <brad.rogan@gmail.com>
2019-12-06 21:56:56 -0700

    updated startup script

--- dingle-bot-startup.sh
+++ dingle-bot-startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-/home/pi/apps/ngrok http --subdomain=dinglebot 5000 > /dev/null &
+echo "Starting ngrok...." > /tmp/dingle-bot-service-ngrok.out 
+/home/pi/apps/ngrok http --subdomain=dinglebot 5000 >> /tmp/dingle-bot-service-ngrok.out 2>&1 &
 
-/usr/bin/python3 /home/pi/apps/dingle-bot/dingle-bot.py > /dev/null &
+echo "Starting dingle-bot..." > /tmp/dingle-bot-service.out 
+/usr/bin/python3 /home/pi/apps/dingle-bot/dingle-bot.py >> /tmp/dingle-bot-service.out 2>&1 &
