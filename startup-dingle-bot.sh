e7a4f152298e432831c6a2d6c435f803326cd64f
bradrogan <brad.rogan@gmail.com>
2019-12-06 22:22:38 -0700

    service file

--- startup-dingle-bot.sh
+++ startup-dingle-bot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Starting ngrok...." > /tmp/dingle-bot-service-ngrok.out 
+date >> /tmp/dingle-bot-service-ngrok.out
+echo "Starting ngrok...." >> /tmp/dingle-bot-service-ngrok.out 
 /home/pi/apps/ngrok http --subdomain=dinglebot 5000 >> /tmp/dingle-bot-service-ngrok.out 2>&1 &
 
-echo "Starting dingle-bot..." > /tmp/dingle-bot-service.out 
+date >> /tmp/dingle-bot-service.out
+echo "Starting dingle-bot..." >> /tmp/dingle-bot-service.out 
 /usr/bin/python3 /home/pi/apps/dingle-bot/dingle-bot.py >> /tmp/dingle-bot-service.out 2>&1 &
